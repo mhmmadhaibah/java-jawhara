ec00534cfd23778c29d9210ed24570fc6ad26467
mhmmadhaibah <mhmmadhaibah@gmail.com>
2025-06-17 20:44:28 +0700

    checkpoints

---
+++ src/main/java/edu/jawhara/Jawhara.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.jawhara;
+
+import edu.jawhara.view.LoginView;
+
+/**
+ *
+ * @author mhmmadhaibah
+ */
+public class Jawhara
+{
+    public static void main(String[] args)
+    {
+        LoginView loginView = new LoginView();
+        loginView.setVisible(true);
+    }
+}
---
+++ src/main/java/edu/jawhara/model/Loading.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.jawhara.model;
+
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author mhmmadhaibah
+ */
+public class Loading
+{
+    public static void infiniteLoading(JPanel jPanel, String cardName)
+    {
+        CardLayout cardLayout = (CardLayout) jPanel.getLayout();
+        
+        cardLayout.show(jPanel, "infinitePanel");
+        Timer timer = new Timer(200, evt -> {
+            cardLayout.show(jPanel, cardName);
+        });
+        
+        timer.setRepeats(false);
+        timer.start();
+    }
+}
