19dbb4b55ab67f48aee45f7b76be2db1bb7f5ab7
mhmmadhaibah <mhmmadhaibah@gmail.com>
2025-06-25 05:23:04 +0700

    checkpoints

--- src/main/java/edu/jawhara/view/AppView.java
+++ src/main/java/edu/jawhara/view/AppView.java
@@ -301,8 +301,10 @@ public class AppView extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new AppView().setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AppView().setVisible(true);
+            }
         });
     }
 
