af8bc496ccf89eb74287d50dd55ac3de32f20484
mhmmadhaibah <mhmmadhaibah@gmail.com>
2025-06-25 15:36:00 +0700

    checkpoints

--- src/main/java/edu/jawhara/model/Loading.java
+++ src/main/java/edu/jawhara/model/Loading.java
@@ -19,7 +19,7 @@ public class Loading
         CardLayout cardLayout = (CardLayout) jPanel.getLayout();
         
         cardLayout.show(jPanel, "infinitePanel");
-        Timer timer = new Timer(200, evt -> {
+        Timer timer = new Timer(100, evt -> {
             cardLayout.show(jPanel, cardName);
         });
         
