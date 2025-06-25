f581ada577bf5c2ee6b2295997d7ddb09d7667f8
mhmmadhaibah <mhmmadhaibah@gmail.com>
2025-06-25 22:04:43 +0700

    checkpoints

--- jawhara.sql
+++ jawhara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jun 2025 pada 18.12
+-- Waktu pembuatan: 25 Jun 2025 pada 17.01
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
-  `name` varchar(8) NOT NULL
+  `name` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -45,30 +45,54 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `outlets`
+--
+
+CREATE TABLE `outlets` (
+  `id` int(11) NOT NULL,
+  `name` varchar(16) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `outlets`
+--
+
+INSERT INTO `outlets` (`id`, `name`, `description`) VALUES
+(1, 'JS Building', 'Toko Cabang Utama'),
+(2, 'WhatsApp Admin', 'Pesanan Masuk dari WA'),
+(3, 'TikTok Shop', 'Marketplace'),
+(4, 'Tokopedia', 'Marketplace'),
+(5, 'Shopee', 'Marketplace');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `products`
 --
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `name` varchar(25) NOT NULL
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `products`
 --
 
-INSERT INTO `products` (`id`, `category_id`, `name`) VALUES
-(1, 2, 'JENNARA'),
-(2, 2, 'ALICE SERIES'),
-(3, 2, 'EUROPE SERIES'),
-(4, 1, 'GENDHIS'),
-(5, 1, 'KHIMAR MAHESA'),
-(6, 3, 'CALANTA SERIES'),
-(7, 3, 'FLORESTA SERIES'),
-(8, 4, 'ANKARA'),
-(9, 4, 'ZAHRATUNNISA PRAYER'),
-(10, 4, 'KHANZA MARYAM');
+INSERT INTO `products` (`id`, `supplier_id`, `category_id`, `name`) VALUES
+(1, 2, 2, 'JENNARA'),
+(2, 1, 2, 'ALICE SERIES'),
+(3, 1, 2, 'EUROPE SERIES'),
+(4, 1, 1, 'GENDHIS'),
+(5, 1, 1, 'KHIMAR MAHESA'),
+(6, 1, 3, 'CALANTA SERIES'),
+(7, 1, 3, 'FLORESTA SERIES'),
+(8, 1, 4, 'ANKARA'),
+(9, 1, 4, 'ZAHRATUNNISA PRAYER'),
+(10, 2, 4, 'KHANZA MARYAM');
 
 -- --------------------------------------------------------
 
@@ -79,7 +103,7 @@ INSERT INTO `products` (`id`, `category_id`, `name`) VALUES
 CREATE TABLE `product_stocks` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -87,16 +111,38 @@ CREATE TABLE `product_stocks` (
 --
 
 INSERT INTO `product_stocks` (`id`, `product_id`, `quantity`) VALUES
-(1, 1, 16),
-(2, 2, 10),
-(3, 3, 20),
-(4, 4, 24),
-(5, 5, 25),
-(6, 6, 15),
-(7, 7, 5),
-(8, 8, 20),
-(9, 9, 25),
-(10, 10, 20);
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0),
+(4, 4, 0),
+(5, 5, 0),
+(6, 6, 0),
+(7, 7, 0),
+(8, 8, 0),
+(9, 9, 0),
+(10, 10, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(16) NOT NULL,
+  `address` text NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `address`, `phone`, `email`) VALUES
+(1, 'Jawhara Boutique', 'Jl. Kincan Raya', '021-2138-6789', 'jawhara.boutique@gmail.com'),
+(2, 'Abaya Dubai', 'Jl. Condet', '021-9442-2444', 'abaya.dubai@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -107,17 +153,11 @@ INSERT INTO `product_stocks` (`id`, `product_id`, `quantity`) VALUES
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `outlet_id` int(11) DEFAULT NULL,
   `type` enum('IN','OUT') NOT NULL,
+  `notes` text DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `transactions`
---
-
-INSERT INTO `transactions` (`id`, `user_id`, `type`, `timestamp`) VALUES
-(1, 5, 'IN', '2025-06-22 07:32:01'),
-(2, 6, 'OUT', '2025-06-22 15:38:06');
 
 -- --------------------------------------------------------
 
@@ -132,24 +172,6 @@ CREATE TABLE `transaction_details` (
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `transaction_details`
---
-
-INSERT INTO `transaction_details` (`id`, `transaction_id`, `product_id`, `quantity`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 10),
-(3, 1, 3, 20),
-(4, 1, 4, 25),
-(5, 1, 5, 25),
-(6, 1, 6, 15),
-(7, 1, 7, 5),
-(8, 1, 8, 20),
-(9, 1, 9, 25),
-(10, 1, 10, 20),
-(11, 1, 1, 15),
-(12, 2, 4, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -158,6 +180,7 @@ INSERT INTO `transaction_details` (`id`, `transaction_id`, `product_id`, `quanti
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `role` enum('Admin','Staff') NOT NULL DEFAULT 'Staff',
   `username` varchar(16) NOT NULL,
   `password` varchar(32) NOT NULL
@@ -167,15 +190,16 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `role`, `username`, `password`) VALUES
-(1, 'Admin', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(2, 'Admin', 'haibah', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(3, 'Staff', 'dany', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(4, 'Staff', 'iwan', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(5, 'Staff', 'tasya', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(6, 'Staff', 'mita', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(7, 'Staff', 'dandy', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(8, 'Staff', 'windy', '5f4dcc3b5aa765d61d8327deb882cf99');
+INSERT INTO `users` (`id`, `name`, `role`, `username`, `password`) VALUES
+(1, 'Admin', 'Admin', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(2, 'Muhammad Haibah', 'Admin', 'haibah', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(3, 'Dandy Septiawan', 'Staff', 'dandy', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(4, 'Taufik Laleno', 'Staff', 'topik', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(5, 'Muhammad Dwi Kurniawan', 'Staff', 'iwan', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(6, 'Dany Primero', 'Staff', 'dany', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(7, 'Paramita Susanti', 'Staff', 'mita', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(8, 'Tasya Laila Syakira', 'Staff', 'tasya', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(9, 'Arwindy Reviana', 'Staff', 'windy', '5f4dcc3b5aa765d61d8327deb882cf99');
 
 --
 -- Indexes for dumped tables
@@ -188,6 +212,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `outlets`
+--
+ALTER TABLE `outlets`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `products`
 --
 ALTER TABLE `products`
@@ -197,6 +227,12 @@ ALTER TABLE `products`
 -- Indeks untuk tabel `product_stocks`
 --
 ALTER TABLE `product_stocks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `suppliers`
+--
+ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -228,6 +264,12 @@ ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT untuk tabel `outlets`
+--
+ALTER TABLE `outlets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
@@ -240,22 +282,28 @@ ALTER TABLE `product_stocks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT untuk tabel `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaction_details`
 --
 ALTER TABLE `transaction_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
